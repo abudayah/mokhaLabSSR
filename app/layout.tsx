3a2fbdae1e08c996ebd93587343dc32925999d06
Anas Abu dayah <anas.abudayah@gmail.com>
2026-04-24 12:18:30 -0700

    add facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,29 @@ gtag('config', '${GA_ID}');
             `}
           </Script>
 
+          {/* Meta Pixel */}
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','981881587864717');
+fbq('track','PageView');
+            `}
+          </Script>
+          <noscript>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=981881587864717&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+
           {children}
 
           {/* Consent banner — renders client-side, hidden once user has chosen */}
